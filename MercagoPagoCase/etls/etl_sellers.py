0476ca73ad42ec2d49b8eaa889cf5e5d813fcf15
FranciscoMMonteiro <112831777+FranciscoMMonteiro@users.noreply.github.com>
2025-11-13 02:25:16 -0300

    Implement token expiration check and auto-refresh
    Added logic to check token expiration and refresh if necessary. Removed user input for refreshing tokens and added timestamp tracking.

--- MercagoPagoCase/etls/etl_sellers.py
+++ MercagoPagoCase/etls/etl_sellers.py
@@ -6,7 +6,7 @@ from utils.api_fuctions import get_sellers
 with open("site_ids.json", "r", encoding="utf-8") as f:
     site_ids = json.load(f)
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from database.database_connection import get_bigquery_client
 from google.cloud import bigquery
 import time
@@ -17,14 +17,25 @@ def main():
     CLIENT_SECRET = os.getenv('CLIENT_SECRET')
     REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
     ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    with open("last_token_time.json", "r", encoding="utf-8") as last_token_file:
+        last_token_time = json.load(last_token_file)["last_token_time"]
 
-    get_new_token = input('Do you want to get a new refresh token? (y/n): ')
-    if get_new_token.lower() == 'y':
+
+    if datetime.now() - datetime.fromisoformat(last_token_time) > timedelta(hours=4):
+        #get_new_token = input('Do you want to get a new refresh token? (y/n): ')
+        get_new_token = True
+    else:
+        get_new_token = False
+        
+    if get_new_token:
         new_access_token, new_refresh_token = generate_new_tokens(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
         update_env_token('ACCESS_TOKEN',new_access_token)
         update_env_token('REFRESH_TOKEN',new_refresh_token)
         ACCESS_TOKEN = new_access_token
         REFRESH_TOKEN = new_refresh_token
+        last_token_time = datetime.now().isoformat()
+        with open("last_token_time.json", "w", encoding="utf-8") as f:
+            f.write('{"last_token_time" :'+ '"' + last_token_time+'"}')
 
     bigquery_client = get_bigquery_client()
     query = """
@@ -72,7 +83,8 @@ def main():
     job_run_timestamp = datetime.now()
     df_sellers['job_run_timestamp'] = job_run_timestamp
 
-    upload = input('Do you want to upload the seller table to the database? (y/n) ')
+    upload = 'y'
+    #upload = input('Do you want to upload the seller table to the database? (y/n) ')
     if upload == 'y':
         bigquery_client = get_bigquery_client()
         table_seller = "mercadolivrecase.sellers"
